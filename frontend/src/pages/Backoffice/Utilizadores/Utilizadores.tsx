e92567b88c89a8e09c62ff4189edbe7773869b24
Leeonealves <125828959+Leeonealves@users.noreply.github.com>
2026-01-16 11:00:16 +0000

    CSS atualizado em Utilizadores e modal Criar Pergunta

--- frontend/src/pages/Backoffice/Utilizadores/Utilizadores.tsx
+++ frontend/src/pages/Backoffice/Utilizadores/Utilizadores.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchUtilizadores, updateUtilizador } from "../../../api/utilizadores";
 import "./Utilizadores.css";
 import { useNavigate } from "react-router-dom";
